0a8cfc03e769cb70a0deec097cb20150cb0a42fa
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2023-04-26 01:05:53 -0300

    excepcion de no tener estaciones en una linea

--- src/main/java/ar/edu/utn/frba/dds/domain/serviciospublicos/Linea.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/serviciospublicos/Linea.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frba.dds.domain.serviciospublicos;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,4 +11,12 @@ public class Linea {
   Estacion origen;
   Estacion destino;
   List<Estacion> estaciones;
+  public Linea(List<Estacion> estaciones, Estacion origen, Estacion destino) {
+    if (!estaciones.contains(origen) || !estaciones.contains(destino)) {
+      throw new IllegalArgumentException("La lista de estaciones debe incluir el origen y el destino.");
+    }
+    this.estaciones = estaciones;
+    this.origen = origen;
+    this.destino = destino;
+  }
 }
