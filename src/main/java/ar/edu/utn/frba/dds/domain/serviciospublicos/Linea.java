4cc4afbb289b3f0bb2fca3ac4e275a360a0c669f
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2023-04-26 12:43:49 -0300

    linea y estaciones

--- src/main/java/ar/edu/utn/frba/dds/domain/serviciospublicos/Linea.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/serviciospublicos/Linea.java
@@ -7,16 +7,16 @@ import lombok.Setter;
 
 @Setter @Getter
 public class Linea {
-  Character nombre;
-  Estacion origen;
-  Estacion destino;
-  List<Estacion> estaciones;
+  private Character nombre;
+  private Estacion origen;
+  private Estacion destino;
+  private List<Estacion> estaciones;
   public Linea(List<Estacion> estaciones, Estacion origen, Estacion destino) {
     if (!estaciones.contains(origen) || !estaciones.contains(destino)) {
       throw new IllegalArgumentException("La lista de estaciones debe incluir el origen y el destino.");
     }
-    this.estaciones = estaciones;
     this.origen = origen;
     this.destino = destino;
+    this.estaciones=estaciones;
   }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,12 +10,16 @@ public class main {
 
     System.out.println( "Prueba clave sin mayuscula");
     System.out.println( validador.esValida("holaaaaaaaaaaaa2"));
+
     System.out.println( "Prueba clave sin minuscula");
     System.out.println( validador.esValida("HOLAAAAAAAAA2"));
+
     System.out.println( "Prueba clave de menos de 8 caracteres");
     System.out.println( validador.esValida("holA2"));
+
     System.out.println( "Prueba clave entre mas buscadas");
     System.out.println( validador.esValida("1Qaz2wsx"));
+
     System.out.println( "Prueba clave valida");
     System.out.println( validador.esValida("VamosAPromocionarCon10$"));
 
