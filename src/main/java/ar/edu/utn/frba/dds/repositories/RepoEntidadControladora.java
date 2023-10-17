0e5cfc446a78fc96068836f8bade4a54dc5326bc
JoseNarvaja <josegastonnarvaja@gmail.com>
2023-10-17 20:51:01 -0300

    Se agrega carga masiva en Entidades Controladoras

--- src/main/java/ar/edu/utn/frba/dds/repositories/RepoEntidadControladora.java
+++ src/main/java/ar/edu/utn/frba/dds/repositories/RepoEntidadControladora.java
@@ -1,5 +1,20 @@
 package ar.edu.utn.frba.dds.repositories;
 
-public class RepoEntidadControladora {
+import ar.edu.utn.frba.dds.models.domain.entidadesExtra.EntidadControladora;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 
+import javax.persistence.EntityTransaction;
+import java.util.List;
+
+public class RepoEntidadControladora implements WithSimplePersistenceUnit {
+    public void agregar(EntidadControladora entidadControladora){
+        EntityTransaction tx = entityManager().getTransaction();
+        tx.begin();
+        entityManager().persist(entidadControladora);
+        tx.commit();
+    }
+
+    public List<EntidadControladora> buscarTodos(){
+        return entityManager().createQuery("from " + EntidadControladora.class.getName()).getResultList();
+    }
 }
