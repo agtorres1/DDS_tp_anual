1987d5799bc2e8be2504c44caf4fac4612fed5e5
enferd <fandrade@frba.utn.edu.ar>
2023-10-24 17:19:22 -0300

    Arreglos al servicio 3

--- src/main/java/ar/edu/utn/frba/dds/models/domain/services_api/service_3/ServicioFusionador.java
+++ src/main/java/ar/edu/utn/frba/dds/models/domain/services_api/service_3/ServicioFusionador.java
@@ -1,9 +1,9 @@
 package ar.edu.utn.frba.dds.models.domain.services_api.service_3;
 
-import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.RequestComunidadesAnalizables;
-import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.RequestComunidadesFusionables;
-import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.ResponseComunidadFusionada;
-import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.ResponseComunidadesAnalizables;
+import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.requests.RequestComunidadesAnalizables;
+import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.requests.RequestComunidadesFusionables;
+import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.responses.ResponseComunidadFusionada;
+import ar.edu.utn.frba.dds.models.domain.services_api.service_3.entities.responses.ResponseComunidadesAnalizables;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
